b3d38f5ad0ce8057f1fbdfd52a90fad961103167
Mark Heimann <mheimann@wustl.edu>
2015-02-06 23:28:36 -0600

    a few more functions used during HW 2

--- code_Math4351hw1.py
+++ code_Math4351hw1.py
@@ -51,7 +51,7 @@ def decode_substitution_manually(code, codeLetter, decodedLetter):
 #Implementation of the Euclidean algorithm for computing the gcd of two positive integers a,b
 def euclidean_gcd(divisor,dividend):
 	#make sure input is valid and in a form we want it
-	divisor,b = format_input(divisor,dividend)
+	divisor,b = formatInput_Euclidean(divisor,dividend)
 	
 	#base case: dividend divides divisor
 	if dividend % divisor == 0:
@@ -64,7 +64,7 @@ def euclidean_gcd(divisor,dividend):
 #Implementation of extended Euclidean algorithm for finding u,v such that ua + vb = gcd(a,b)
 def extended_Euclidean(divisor,dividend):
 	#make sure input is valid and in a form we want it
-	divisor,b = format_input(divisor,dividend)
+	divisor,b = formatInput_Euclidean(divisor,dividend)
 	return extended_Euclidean_rec(1,divisor,0,dividend,divisor,dividend)
 
 #Solves not only for the gcd of two positive integers a and b
@@ -86,7 +86,7 @@ def extended_Euclidean_rec(u,g,x,y,a,b):
 		return extended_Euclidean_rec(u,g,x,y,a,b)
 
 #Formats input for Euclidean and extended Euclidean algorithms
-def format_input(a,b):
+def formatInput_Euclidean(a,b):
 	#check for bad input
 	if a <= 0 or b <= 0:
 		raise ValueError("Both numbers must be positive")
@@ -100,3 +100,38 @@ def format_input(a,b):
 		b = temp
 		
 	return a,b
+	
+#see if testRoot is a primitive root of b
+#see if testRoot^k = any nonnegative integer less than b for some k
+def primitivity_test(testRoot,modulus):
+	if testRoot > modulus: 
+		temp = testRoot
+		testRoot = modulus
+		modulus = temp
+	if modulus <= 1:
+		raise ValueError("You must take roots of an integer greater than 1")
+	if type(testRoot) != int or type(modulus) != int:
+		raise TypeError("You must pass in integers")
+		
+	roots = list() #hold roots
+	#note: since a^(b-1) = 1 = a^0 mod b by Fermat's Little Theorem, only need to check through 1 to b - 1
+	#note: no repeats since there are the same number of integers modulo b and numbers between 1 and b-1
+	#where b is the modulus
+	for i in range(1,modulus):
+		roots.append(testRoot**i % modulus)
+	roots = sorted(roots)
+	return roots == [i for i in range(1,modulus)]
+	
+#finds all primitive roots of an integer modulus
+#basically performs a primitivity test for every number less than the inputted integer
+def find_primitive_roots(modulus):
+	roots = []
+	if modulus <= 1:
+		raise ValueError("You must pass in an integer greater than 1")
+	if type(modulus) != int:
+		raise TypeError("You must pass in an integer")
+	for i in range(2,modulus):
+		if primitivity_test(i,modulus):
+			roots.append(i)
+	return roots
+
